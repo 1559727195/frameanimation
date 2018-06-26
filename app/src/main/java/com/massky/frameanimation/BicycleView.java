e80b32e40b11160377fee4f6c01fe2c0d6a8b2e9
zhu <zhu@gmail.com>
2018-06-26 13:53:32 +0800

    添加补间动画

--- app/src/main/java/com/massky/frameanimation/BicycleView.java
+++ app/src/main/java/com/massky/frameanimation/BicycleView.java
@@ -1,17 +1,36 @@
 package com.massky.frameanimation;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 
 /**
  * Created by zhu on 2018/6/26.
  */
 
 public class BicycleView extends View {
+
+    private Animation mAnimationAlpha = null;
+
+    private Animation mAnimationScale = null;
+
+    private Animation mAnimationTranslate = null;
+
+    private Animation mAnimationRotate = null;
+
+    private AnimationSet as = null;
+
+    Bitmap mBicycle = null;
+
+
     public BicycleView(Context context) {
         super(context);
+
+        
     }
 
     public BicycleView(Context context, @Nullable AttributeSet attrs) {
