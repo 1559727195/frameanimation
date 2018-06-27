f2c049f77766a94860fe74b04b8496e9fc929cd5
zhu <zhu@gmail.com>
2018-06-27 10:00:57 +0800

    补间动画tween->animation

--- app/src/main/java/com/massky/frameanimation/BicycleView.java
+++ app/src/main/java/com/massky/frameanimation/BicycleView.java
@@ -2,12 +2,18 @@ package com.massky.frameanimation;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 
 /**
  * Created by zhu on 2018/6/26.
@@ -34,6 +40,39 @@ public class BicycleView extends View {
                 R.drawable.ic_launcher_background
         )).getBitmap();
         //
+        mAnimationAlpha = new AlphaAnimation(
+                0.1f, 1.0f
+        );
+        mAnimationAlpha.setDuration(1000 * 4);
+
+        mAnimationScale = new ScaleAnimation(
+                0.0f, 1.0f, 0.0f, 1.0f,
+                Animation.RELATIVE_TO_SELF, 0.7f,
+                Animation.RELATIVE_TO_SELF, 0.7f
+        );
+
+        mAnimationScale.setDuration(1000 * 4);
+
+        mAnimationTranslate = new TranslateAnimation(
+                5, 200, 4, 150
+        );
+
+        mAnimationTranslate.setDuration(1000 * 6);
+
+        mAnimationRotate = new RotateAnimation(
+                0.0f, +360.0f,
+                Animation.RELATIVE_TO_SELF, 0.3f,
+                Animation.RELATIVE_TO_SELF, 0.3f
+        );
+
+        mAnimationRotate.setDuration(1000 * 4);
+
+        as = new AnimationSet(true);
+        as.addAnimation(mAnimationAlpha);
+        as.addAnimation(mAnimationScale);
+        as.addAnimation(mAnimationTranslate);
+        as.addAnimation(mAnimationRotate);
+        as.setDuration(1000 * 4);
 
     }
 
@@ -43,5 +82,37 @@ public class BicycleView extends View {
 
     public BicycleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(mBicycle, 0, 0, null);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_DPAD_UP:
+
+                break;
+            case KeyEvent.KEYCODE_DPAD_DOWN:
+
+                break;
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+
+                break;
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+
+                break;
+            case KeyEvent.KEYCODE_DPAD_CENTER:
+
+                break;
+
+        }
+
+
+        return super.onKeyUp(keyCode, event);
     }
 }
